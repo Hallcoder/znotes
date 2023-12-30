1a855b3296a1d576bbe34e06530704994aedb834
Hallcoder <hallcoder25@gmail.com>
2023-12-30 14:06:01 +0200

    [feat]: shared preferences config

--- lib/components/CustomGridView.dart
+++ lib/components/CustomGridView.dart
@@ -1,17 +1,29 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:znotes/components/NoteCard.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CustomGrid extends StatefulWidget {
   const CustomGrid({Key? key, required this.count}) : super(key: key);
   final int count;
-
+  final String filter;
   @override
   State<CustomGrid> createState() => _CustomGridState();
 }
 
 class _CustomGridState extends State<CustomGrid> {
+  late final List notes;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNotes();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
+    return notes.isNotEmpty ? GridView.builder(
       itemCount: widget.count,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
@@ -20,8 +32,25 @@ class _CustomGridState extends State<CustomGrid> {
         childAspectRatio: 0.7,
       ),
       itemBuilder: (BuildContext context, int index) {
-
+        return buildNoteCard(notes[index]);
       },
-    );
+    ): const Text("No notes to show!");
+  }
+
+  Widget buildNoteCard(not) {
+    return NoteCard(
+        categoryType: not.categoryType,
+        isPinned: not.isPinned,
+        isStarred: not.isStarred,
+        contents: not.contents,
+        dueDate: not.dueDate);
+  }
+
+  void fetchNotes() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? noteListString = prefs.getString('notes');
+    if (noteListString != null) {
+      notes = json.decode(noteListString);
+    }
   }
 }
